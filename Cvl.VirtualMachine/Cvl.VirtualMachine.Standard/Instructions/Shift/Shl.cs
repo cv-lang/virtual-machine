a83a609bb537fa299f571f5f6da62cf3dc7f6cf0
micje32 <jmichaj.mail@gmail.com>
2021-03-16 15:03:34 +0100

    added shift and switch

---
+++ Cvl.VirtualMachine/Cvl.VirtualMachine.Standard/Instructions/Shift/Shl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvl.VirtualMachine.Instructions.Shift
+{
+    class Shl : InstructionBase
+    {
+        public override void Wykonaj()
+        {
+            dynamic a = PopObject();
+            dynamic b = PopObject();
+
+            dynamic wynik = a << b;
+            PushObject(wynik);
+            WykonajNastepnaInstrukcje();
+        }
+    }
+}
