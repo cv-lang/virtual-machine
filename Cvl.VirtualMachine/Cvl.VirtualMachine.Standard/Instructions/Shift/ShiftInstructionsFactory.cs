b548e441f875cc14c2c87cd4bb3f1189dd1fc76b
micje32 <jmichaj.mail@gmail.com>
2021-03-16 15:03:34 +0100

    added shift and switch

---
+++ Cvl.VirtualMachine/Cvl.VirtualMachine.Standard/Instructions/Shift/ShiftInstructionsFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Reflection;
+
+namespace Cvl.VirtualMachine.Instructions.Shift
+{
+    public class ShiftInstructionsFactory : InstructionFactory
+    {
+        public override InstructionBase CreateInstruction(Instruction instrukcja)
+        {
+            switch (instrukcja.OpCode.Name)
+            {
+                case "shl":
+                    return CreateInstruction<Shl>(instrukcja);
+                case "shr":
+                    return CreateInstruction<Shr>(instrukcja);
+            }
+            return null;
+        }
+    }
+}
