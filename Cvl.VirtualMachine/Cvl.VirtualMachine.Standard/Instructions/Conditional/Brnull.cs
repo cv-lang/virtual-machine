7841041156822dfde0168b9b9d7ef870a39a1b07
micje32 <jmichaj.mail@gmail.com>
2021-03-18 10:55:41 +0100

    added switch brzero brnull brinst stelem ldind stind

---
+++ Cvl.VirtualMachine/Cvl.VirtualMachine.Standard/Instructions/Conditional/Brnull.cs
@@ -0,0 +1,37 @@
+﻿using Mono.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvl.VirtualMachine.Instructions.Conditional
+{
+    //Transfers control to a target instruction if value is null.
+    public class Brnull : InstructionBase
+    {
+        public override void Wykonaj()
+        {
+            var wynik = false;
+            dynamic a = PopObject();
+            if (a == null)
+            {
+                wynik = false;
+            } 
+            else
+            {
+                wynik = true;
+            }
+
+
+            if (wynik == false)
+            {
+                var op = Instruction.Operand as Instruction;
+                var nextOffset = op.Offset;
+                WykonajSkok(nextOffset);
+            }
+            else
+            {
+                WykonajNastepnaInstrukcje();
+            }
+        }
+    }
+}
---
+++ Cvl.VirtualMachine/Cvl.VirtualMachine.Standard/Instructions/Special/Ldind.cs
@@ -0,0 +1,44 @@
+﻿using Mono.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvl.VirtualMachine.Instructions.Storage
+{
+    public class LdindFactory : InstructionFactory
+    {
+        public override InstructionBase CreateInstruction(Instruction instrukcja)
+        {
+            switch (instrukcja.OpCode.Name)
+            {
+                case "ldind"://TODO:Testy 
+                case "ldind.i":
+                case "ldind.i2":
+                case "ldind.i4":
+                case "ldind.i8":
+                case "ldind.r4":
+                case "ldind.r8":
+                case "ldind.ref":
+                case "ldind.u1":
+                case "ldind.u2":
+                case "ldind.u4":
+                case "ldind.u8":
+                    return CreateInstruction<Ldind>(instrukcja);
+            }
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Loads the element at a specified array index onto the top of the evaluation stack as the type specified in the instruction.
+    /// https://docs.microsoft.com/pl-pl/dotnet/api/system.reflection.emit.opcodes.ldelem?view=net-5.0
+    /// </summary>
+    public class Ldind : InstructionBase
+    {
+
+        public override void Wykonaj()
+        {
+            WykonajNastepnaInstrukcje();
+        }
+    }
+}
